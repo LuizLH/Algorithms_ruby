ee063f893eaa2178f569db5ac5afea7a2daaa0e3
LuizLH <luizlh@gmail.com>
2022-11-28 21:53:00 -0300

    feat: implementing Kruskal Minimum Spanning Tree

--- minimum_spanning_tree_test.rb
+++ minimum_spanning_tree_test.rb
@@ -1,2 +1,21 @@
 #Testing Kruskal’s Minimum Spanning Tree Algorithm
 
+g = Graphbox.new(4)
+
+# Edge 0-1
+g.addEdge(0, 1, 10)
+
+# Edge 0-2
+g.addEdge(0, 2, 6)
+
+# Edge 0-3
+g.addEdge(0, 3, 5)
+
+# Edge 1-3
+g.addEdge(1, 3, 15)
+
+# Edge 2-3
+g.addEdge(2,3,4)
+
+g.KruskalMST()
+
