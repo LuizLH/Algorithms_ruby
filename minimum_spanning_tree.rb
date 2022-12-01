42d7d96029073741f495cadd2cec05fa39a2dc96
LuizLH <luizlh@gmail.com>
2022-11-30 23:27:13 -0300

    feat: Kruskal minimum tree

--- minimum_spanning_tree.rb
+++ minimum_spanning_tree.rb
@@ -12,24 +12,48 @@ class Graph
         @graph.append([src, dest, weight])
     end
 
+    def partition(p, rr)
+        x = R[p]
+        i = p - 1
+        j = rr + 1
+        while i < j 
+            until R[j]<=x
+                j:=j-1
+            end
+            
+            until R[i]>=x
+                i:=i+1
+            end
 
+            if i < j
+                t    = R[i] 
+                R[i] = R[j] 
+                R[j] = t
+
+                z    = B[i] 
+                B[i] = B[j] 
+                B[j] = z
+                
+                z    = E[i]
+                E[i] = E[j]
+                E[j] = z
+            end
+        end
+      Partition = j
+    end
+
+    def QuicksortR(p, rr)
+        if p < rr
+            q = Partition(p,rr)
+            QuicksortR(p,q)
+            QuicksortR(q+1,rr)
+        end
+    end
     
     def KruskalMST()
 
-        # This will store the resultant MST
-        result = Array.new
-
-        # An index variable, used for sorted edges
-        i = 0 
- 
-        # An index variable, used for result[]
-        e = 0
-
-        while ( e < @vertices - 1)
-
-        end
-    
-
+        QuicksortR(1,n);
+        #MinTree;
 
     end
 
