97f234810625081af4c10d95095ad7692acff1f2
LuizLH <luizlh@gmail.com>
2022-12-09 23:48:01 -0300

    feat: implement and test another quicksort version

--- quick_sort.rb
+++ quick_sort.rb
@@ -13,10 +13,11 @@ class QuickSort
 
         i = l
         j = r
-        puts "l = #{l} , r = #{r}"
-        pivot = @list[(l + r) >> l]
-        puts pivot
-        until i > j
+
+        pivot = @list[(l + r) / 2]
+        puts "Pivot = #{pivot}"
+        begin
+            puts "i = #{i} , j = #{j}"
             while @list[i] < pivot 
                 i += 1 
             end
@@ -32,9 +33,39 @@ class QuickSort
                 i += 1 
                 j -= 1
             end
-        end
+            puts "i = #{i} , j = #{j}"
+        end until !(i > j)
         qsort(l, j)
         qsort(i, r) 
     end
+
+    # The quicksort method takes an array of elements and returns a sorted array
+    def quicksort(elements)
+        # If the length of the array is 0 or 1, the array is already sorted
+        return elements if elements.length <= 1
+    
+        # Select the first element of the array as the pivot
+        pivot = elements.first
+    
+        # Create two arrays to hold the elements that are less than and greater than the pivot
+        less_than_pivot = []
+        greater_than_pivot = []
+    
+        # Loop through the rest of the elements in the array
+        elements[1..-1].each do |element|
+        # If the element is less than the pivot, add it to the less_than_pivot array
+        # Otherwise, add it to the greater_than_pivot array
+        if element <= pivot
+            less_than_pivot << element
+        else
+            greater_than_pivot << element
+        end
+        end
+    
+        # Recursively sort the less_than_pivot and greater_than_pivot arrays
+        # and concatenate the result with the pivot in the middle
+        quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+    end
+
 
 end
--- quick_sort_test.rb
+++ quick_sort_test.rb
@@ -1,12 +1,28 @@
 #Testing binary search algorithm
 require "./quick_sort.rb"
+require "benchmark" 
 
-lista = [5, 1, 4, 2, 8]
+#lista = [5, 1, 4, 2, 8]
+lista = [-34, -20, 30, 13, 36, -10, 5, -25, 9, 19, 35, -50, 29, 11]
 
 qsort = QuickSort.new(lista)
 
-p lista
+#p lista
 
-qsort.qsort(0, lista.length-1)
+#qsort.qsort(0, lista.length-1)
 
-p lista
+#p lista
+
+=begin
+result = Benchmark.measure do
+    # Create an array of unsorted numbers
+    numbers = [4, 7, 2, 1, 3, 8, 5, 9, 6]
+    
+    # Sort the array using the quicksort method
+    sorted_numbers = qsort.quicksort(numbers)
+
+    puts "Output: #{sorted_numbers}"
+end
+
+puts result
+=end
