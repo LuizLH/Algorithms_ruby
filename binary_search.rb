dd42742bfa45f9d474298a6bdfc24b880f4afdf4
LuizLH <luizlh@gmail.com>
2022-12-13 23:33:25 -0300

    fix: style guide

--- binary_search.rb
+++ binary_search.rb
@@ -1,46 +1,39 @@
-#Implementing binary search algorithm
+# Implementing binary search algorithm
 module BinarySearch
-
-    #Iterative method
-    def BinarySearch.iterative(list, element)
-        low  = 0
-        high = list.length-1
-        mid  = 0
-
-        until low == high
-            mid = (low + high)/2
-        
-            if (element == list[mid])
-                break
-            elsif (element > list[mid]) # element is on the right side"
-                mid = mid + 1
-                low = mid
-            else
-                mid  = mid -1           # element is on the left side
-                high = mid 
-            end
-        end
-        if (element == list[mid])
-            return mid
-        else
-            return -1
-        end
+  # Iterative method
+  def self.iterative(list, element)
+    low = mid = 0
+    high = list.length - 1
+    until low == high
+      mid = (low + high) / 2
+      if element == list[mid]
+        break
+      elsif element > list[mid] # element is on the right side
+        mid += 1
+        low = mid
+      else
+        mid -= 1 # element is on the left side
+        high = mid
+      end
     end
-
-    #Recursive method
-    def BinarySearch.recursivo(list, element, low, high)
-        if low > high
-            return -1 
-        else
-            mid = (low + high) / 2 
-            if element == list[mid]
-                return mid
-            elsif element > list[mid]    # element is on the right side
-                return recursivo(list, element, mid + 1, high)
-            else                         # element is on the left side
-                return recursivo(list, element, low, mid - 1)
-            end
-        end
+    if element == list[mid]
+      mid
+    else
+      -1
     end
+  end
 
+  # Recursive method
+  def self.recursivo(list, element, low, high)
+    return -1 if low > high
+
+    mid = (low + high) / 2 
+    if element == list[mid]
+      mid
+    elsif element > list[mid]    # element is on the right side
+      recursivo(list, element, mid + 1, high)
+    else                         # element is on the left side
+      recursivo(list, element, low, mid - 1)
+    end
+  end
 end
--- binary_search_test.rb
+++ binary_search_test.rb
@@ -1,9 +1,9 @@
-#Testing binary search algorithm
-require "./binary_search.rb"
+# Testing binary search algorithm
+require './binary_search'
 
-#Teste Lista de inteiros 
+# Teste Lista de inteiros
 lista_inteiros = [10, 20, 30, 50, 60, 80, 100, 110, 130, 170]
 
-puts "Iterative binary search at position #{BinarySearch::iterative(lista_inteiros, 170)}"
+puts "Iterative binary search at position #{BinarySearch.iterative(lista_inteiros, 170)}"
 
-puts "Recursive binary search at position #{BinarySearch::recursivo(lista_inteiros, 170, 0, lista_inteiros.length-1)}"
+puts "Recursive binary search at position #{BinarySearch.recursivo(lista_inteiros, 170, 0, lista_inteiros.length - 1)}"
